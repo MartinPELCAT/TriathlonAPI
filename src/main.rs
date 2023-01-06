40dcbd0d9f782c09a1dbca5865158507d2fdd0a7
Martin PELCAT <m.pelcat@numendo.com>
2023-01-06 15:26:51 +0100

    add challenge familly

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 pub mod services;
 
-use actix_web::{App, HttpServer};
+use actix_web::{middleware, App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("server started at 127.0.0.1::6969");
-    
-    HttpServer::new(|| App::new().service(services::ironman::get_ironmans))
-        .bind(("127.0.0.1", 6969))?
-        .run()
-        .await
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Compress::default())
+            .service(services::ironman::get_ironmans)
+            .service(services::challenge::get_challenges)
+    })
+    .bind(("127.0.0.1", 6969))?
+    .run()
+    .await
 }
